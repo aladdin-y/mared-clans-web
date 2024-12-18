7b9d728d30bd95d270c8199a454a6c9f4eaf446a
Aladdin <145193322+aladdin-y@users.noreply.github.com>
2024-12-18 16:47:25 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    i18n: {
-      defaultLocale: 'en', // اللغة الافتراضية
-      locales: ['ar', 'en'], // اللغات المدعومة
-    },
-  
+
 };
 
 export default nextConfig;
